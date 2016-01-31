6eb0d25713808534ecf2297a1a965c76cb72c70b
Asrar <azahmed@gmail.com>
2016-01-31 11:45:27 +1100

    Created DisplayMessageActivity

--- app/src/main/java/com/example/asrar/myapplication/MyActivity.java
+++ app/src/main/java/com/example/asrar/myapplication/MyActivity.java
@@ -1,5 +1,6 @@
 package com.example.asrar.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.asrar.myapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,13 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
